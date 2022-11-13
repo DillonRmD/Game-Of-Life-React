64548d1d5d5169635789aee33ec1eb908248914c
DillonRmD <dillonwilliams2001@yahoo.com>
2022-11-13 15:56:09 -0600

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,59 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+const App = () => {
+
+  const [grid, setGrid] = useState<number [][]>(
+    [
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0],
+    ]
+  );
+
+  const [gameStarted, setGameStarted] = useState<boolean>(false);
+  const [gameText, setGameText] = useState<string>("Start Game");
+
+  function setGridValue(rowIndex: number, colIndex: number, value: number) {
+    const newGrid = [...grid];
+    newGrid[rowIndex][colIndex] = value;
+    setGrid(newGrid);
+  }
+
+  function userChangeGridValue(rowIndex: number, colIndex: number, value: number){
+    if(gameStarted == true)
+      return;
+    
+    setGridValue(rowIndex, colIndex, value);
+  }
+
+  function startGame(){
+    if(gameStarted == true){
+      setGameText("Start Game");
+      setGameStarted(false);
+    }
+    else if(gameStarted == false){
+      setGameText("Stop Game");
+      setGameStarted(true);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={startGame} id="">{gameText}</button>
+      <div className="grid">
+        {grid.map((row, rowIndex) => (
+          <div key={rowIndex} className="row">
+            {row.map((value, colIndex) => (
+              <div key={colIndex} className="cell" onClick={() => userChangeGridValue(rowIndex, colIndex, 1)}>{value}</div>
+            ))}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
